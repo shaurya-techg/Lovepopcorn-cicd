30383676629ee9a81236ecfeef807cece0fd04be
shaurya-techg <shauryastudent1@gmail.com>
2026-07-02 14:49:09 +0530

    Add Movie model and Django admin configuration

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Movie
 
-# Register your models here.
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "genre",
+        "release_year",
+        "imdb_rating",
+    )
+
+    search_fields = (
+        "title",
+        "genre",
+    )
+
+    list_filter = (
+        "genre",
+        "release_year",
+    )
--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    GENRE_CHOICES = [
+        ("Action", "Action"),
+        ("Adventure", "Adventure"),
+        ("Animation", "Animation"),
+        ("Comedy", "Comedy"),
+        ("Crime", "Crime"),
+        ("Drama", "Drama"),
+        ("Fantasy", "Fantasy"),
+        ("Horror", "Horror"),
+        ("Romance", "Romance"),
+        ("Sci-Fi", "Sci-Fi"),
+        ("Thriller", "Thriller"),
+    ]
+
+    title = models.CharField(max_length=200)
+    genre = models.CharField(max_length=20, choices=GENRE_CHOICES)
+    release_year = models.PositiveIntegerField()
+    imdb_rating = models.DecimalField(max_digits=3, decimal_places=1)
+    description = models.TextField()
+    poster_url = models.URLField(blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
